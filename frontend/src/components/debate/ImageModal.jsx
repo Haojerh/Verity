453de19088c7c30b69952d335d31a81f46ae339c
Haojerh <haojerh@gmail.com>
2026-04-08 19:14:32 +0800

    fixed image modal bug

--- frontend/src/components/debate/ImageModal.jsx
+++ frontend/src/components/debate/ImageModal.jsx
@@ -27,7 +27,11 @@ export default function ImageModal({
   return (
     <div
       className="fixed inset-0 bg-black/95 z-[100] flex items-center justify-center"
-      onClick={onClose}
+      onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            onClose();
+        }}
     >
       {/* Close button */}
       <button
