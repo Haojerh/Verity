925b85f8ef522e7fe825eab8c241df56b6534b16
CTF enthusiast <wongxuanwei3132@gmail.com>
2026-04-25 23:15:10 +0800

    Update import path for DebateCard component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import DebateCard from "../components/debate/DebateCard";
+import DebateCard from "../components/homeDebate/DebateCard";
 
 
 export default function Home() {
