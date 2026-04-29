3b2c61b34a328e8cf3429c3b0737644de1083f02
Haojerh <haojerh@gmail.com>
2026-04-29 18:47:21 +0800

    fixed smth and for pull

--- backend/src/main/java/com/verity/backend/controller/DebateController.java
+++ backend/src/main/java/com/verity/backend/controller/DebateController.java
@@ -1,16 +1,22 @@
 package com.verity.backend.controller;
 
-import com.verity.backend.model.Debate;
-import com.verity.backend.repository.DebateRepository;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.verity.backend.model.Debate;
+import com.verity.backend.repository.DebateRepository;
 
 
 @RestController
@@ -61,3 +67,4 @@ public class DebateController {
         Pageable pageable = PageRequest.of(page, size);
         return debateRepository.searchDebatesPaginated(q, pageable);
     }
+}
