ba339108d21d8e83726770d03a34dce4459704ca
Sia Yu <ongsiayu18@gmail.com>
2026-05-10 17:29:14 +0800

    Cleaned up UserDTO and Request

--- backend/src/main/java/com/Verity/DTO/UserRequest.java
+++ backend/src/main/java/com/Verity/DTO/UserRequest.java
@@ -1,41 +1,19 @@
 package com.Verity.DTO;
-import java.time.LocalDateTime;
-import java.util.List;
+
 
 import org.springframework.web.multipart.MultipartFile;
-
-import com.Verity.Entity.UserFavTopicEntity;
-import com.Verity.Entity.UserNotiEntity;
 
 import lombok.Data;
 
 @Data
 public class UserRequest {
-    private String SYSUNID;
 
-    private String SYSCREATEDBY;
-
-    private String SYSMODIFIEDBY;
-
-    private LocalDateTime SYSCREATEDDATE;
-
-    private LocalDateTime SYSMODIFIEDDATE;
-
-    private Boolean SYSISDELETED;
+    private String email;
+    private String password;
+    private String name;
 
     private String userID;
 
-    private String email;
+    private MultipartFile avatar;
 
-    private String password;
-
-    private String userRole;
-
-    private String name;
-
-    private MultipartFile avatar; 
-
-    private List<UserNotiEntity> userNotiEntityList;
-
-    private List<UserFavTopicEntity> userFavTopicEntityList;
 }
